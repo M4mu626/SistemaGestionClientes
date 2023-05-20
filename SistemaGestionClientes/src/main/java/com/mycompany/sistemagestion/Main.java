5feb8566799a8b48b3dc379cda8397d1408ad27e
M4mu626 <115762471+M4mu626@users.noreply.github.com>
2023-05-19 21:24:06 -0500

    Add files via upload

---
+++ SistemaGestionClientes/src/main/java/com/mycompany/sistemagestion/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemagestion;
+
+import com.mycompany.sistemagestion.forms.Formulario;
+
+/**
+ *
+ * @author M4uro
+ */
+public class Main {
+
+    public static void main (String[] args){
+		Formulario ventana = new Formulario();
+		ventana.show();
+	}
+
+}
